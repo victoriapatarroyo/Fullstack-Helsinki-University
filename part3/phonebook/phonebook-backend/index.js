f254b70e425a0114549a55d7ecde3178196d94d1
Victoria Patarroyo <victoria.patarroyo@gmail.com>
2026-06-09 13:52:12 -0500

    Ejercicio 3.11 Agenda Telefónica Full Stack

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -110,9 +110,11 @@ app.post("/api/persons", (request, response) => {
 });
 
 // 👇 Manejo de rutas SPA (React)
-app.get("/*", (request, response) => {
+// 👇 Manejo de rutas SPA (React) — CORRECTO
+app.use((request, response) => {
   response.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
+
 // Puerto dinámico para Render
 const PORT = process.env.PORT || 3001;
 
